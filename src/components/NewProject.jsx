0f28fb36443535c109f6f04675eb9425b79deca9
Guilherme Rocha <guirocha1938@gmail.com>
2024-02-20 13:07:03 -0300

    feat: Adds validation modal and cancel logic.

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,9 +1,10 @@
 import { useRef } from "react";
 
 import Input from "./Input";
+import Modal from "./Modal";
 
-const NewProject = ({ onAdd }) => {
-
+const NewProject = ({ onAdd, onCancel }) => {
+    const modal = useRef();
     const title = useRef();
     const description = useRef();
     const dueDate = useRef();
@@ -13,6 +14,11 @@ const NewProject = ({ onAdd }) => {
         const enteredDescription = description.current.value;
         const enteredDueDate = dueDate.current.value;
 
+        if(enteredTitle.trim() === "" || enteredDescription.trim() === "" || enteredDueDate.trim() === "") {
+            modal.current.open();
+            return;
+        }
+
         onAdd({
             title: enteredTitle,
             description: enteredDescription,
@@ -21,9 +27,15 @@ const NewProject = ({ onAdd }) => {
     };
 
     return (
-    <div className="w-[35rem] mt-16">
+    <>
+        <Modal ref={modal} buttonCaption="Okay">
+            <h2 className="text-xl font-bold text-stone-700 my-4">Invalid input!</h2>
+            <p className="text-stone-600 mb-4">Oooops... looks like you forgot to enter a value.</p>
+            <p className="text-stone-600 mb-4">Please make sure you provide a valid value for every input field.</p>
+        </Modal>
+        <div className="w-[35rem] mt-16">
         <menu className="flex items-center justify-end gap-4 my-4">
-            <li><button className="text-stone-800 hover:text-stone-950">Cancel</button></li>
+            <li><button className="text-stone-800 hover:text-stone-950" onClick={onCancel} >Cancel</button></li>
             <li><button onClick={handleSave} className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button></li>
         </menu>
         <div>
@@ -31,7 +43,8 @@ const NewProject = ({ onAdd }) => {
             <Input ref={description} label="Description" textarea/>
             <Input type="date" ref={dueDate} label="Due date"/>
         </div>
-    </div>)
+    </div>
+    </>)
 };
 
 export default NewProject;
