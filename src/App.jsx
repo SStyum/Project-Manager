274e03b05af2792b549ccc75f9ae9ddd02795799
Guilherme Rocha <guirocha1938@gmail.com>
2024-02-20 20:51:51 -0300

    feat: Adds project view and possibility to delete a project.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
+import SelectedProject from "./components/SelectedProject";
 
 function App() {
 
@@ -46,17 +47,43 @@ function App() {
     });
   };
 
-  let content;
+  const handleSelectProject = (id) => {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: id,
+      };
+    });
+  };
+
+  const handleDeleteProject = () => {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter((project) => project.id !== prevState.selectedProjectId),
+      };
+    });
+  };
+
+  const selectedProject = projectsState.projects.find((project) => project.id === projectsState.selectedProjectId);
+
+  let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject} />;
 
   if(projectsState.selectedProjectId === null) {
-    content = <NewProject onAdd={{handleAddProject}} onCancel={handleCancelAddProject} />
+    content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
   } else if (projectsState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
   };
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar onStartAddProject={handleStartAddProject} projects={projectsState.projects} />
+      <ProjectsSidebar 
+      onStartAddProject={handleStartAddProject}
+      projects={projectsState.projects}
+      onSelectProject={handleSelectProject}
+      selectedProjectId={selectedProject}
+      />
       {content}
     </main>
   );
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -35,7 +35,7 @@ const NewProject = ({ onAdd, onCancel }) => {
         </Modal>
         <div className="w-[35rem] mt-16">
         <menu className="flex items-center justify-end gap-4 my-4">
-            <li><button className="text-stone-800 hover:text-stone-950" onClick={onCancel} >Cancel</button></li>
+            <li><button className="text-stone-800 hover:text-stone-950" onClick={onCancel}>Cancel</button></li>
             <li><button onClick={handleSave} className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button></li>
         </menu>
         <div>
