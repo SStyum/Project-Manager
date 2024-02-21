2772bfeb7ea6057cd7883344a985d3ef8919d55c
Guilherme Rocha <guirocha1938@gmail.com>
2024-02-20 21:45:37 -0300

    feat: Adds tasks and task deletion for projects.

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,34 @@ function App() {
   const [projectsState, setProjectsState] = useState({
     selectedProjectId: undefined,
     projects: [],
+    tasks: [],
   });
+
+  const handleAddTask = (text) => {
+    setProjectsState(prevState => {
+
+      const taskId = Math.random();
+      const newTask = {
+        text: text,
+        projectId: prevState.selectedProjectId,
+        id: taskId,
+      };
+
+      return {
+        ...prevState,
+        tasks: [...prevState.tasks, newTask],
+      }
+    });
+  };
+
+  const handleDeleteTask = (id) => {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        tasks: prevState.tasks.filter((task) => task.id !== id),
+      };
+    });
+  };
 
   const handleStartAddProject = () => {
     setProjectsState(prevState => {
@@ -68,7 +95,7 @@ function App() {
 
   const selectedProject = projectsState.projects.find((project) => project.id === projectsState.selectedProjectId);
 
-  let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject} />;
+  let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject} onAddTask={handleAddTask} onDeleteTask={handleDeleteTask} tasks={projectsState.tasks} />;
 
   if(projectsState.selectedProjectId === null) {
     content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
@@ -82,7 +109,7 @@ function App() {
       onStartAddProject={handleStartAddProject}
       projects={projectsState.projects}
       onSelectProject={handleSelectProject}
-      selectedProjectId={selectedProject}
+      selectedProjectId={projectsState.selectedProjectId}
       />
       {content}
     </main>
